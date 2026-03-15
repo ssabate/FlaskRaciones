08940c4f50b555e63c6a81083e8d7fd91669a3c2
Santi Sabaté <ssabate@iesebre.com>
2026-03-15 19:58:01 +0100

    Delete raciones/app/config.py

--- raciones/app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-for-raciones'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:postgres@localhost:5432/raciones'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
