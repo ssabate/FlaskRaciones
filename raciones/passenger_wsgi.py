7a1cb4dd72caf8cfbe24d92f8e061eb0e829dd18
Santi Sabate <ssabate@iesebre.com>
2026-03-15 21:08:12 +0100

    Subo version funcional a produccionó

---
+++ raciones/passenger_wsgi.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
+
+from app import create_app
+application = create_app()
